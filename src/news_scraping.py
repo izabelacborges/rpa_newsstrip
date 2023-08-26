c859e84c0a90aaa5161e1cca7ee9f70b8f1108ae
Izabela Borges <to.izabelacborges@gmail.com>
2023-08-25 22:22:18 -0300

    Add dismiss terms method

--- src/news_scraping.py
+++ src/news_scraping.py
@@ -8,5 +8,10 @@ def open_the_website(url):
     browser_lib.open_available_browser(url)
 
 
+def dismiss_terms():
+    terms_button = "class:css-1fzhd9j"
+    browser_lib.click_button(terms_button)
+
+
 def end_task():
     browser_lib.close_all_browsers()
