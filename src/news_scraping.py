adf4ab09d09832c968a19e0154ebe649a9e12989
Izabela Borges <to.izabelacborges@gmail.com>
2023-08-25 22:20:56 -0300

    Clean up scraping file

--- src/news_scraping.py
+++ src/news_scraping.py
@@ -1,5 +1,6 @@
 from RPA.Browser.Selenium import Selenium
 
+
 browser_lib = Selenium()
 
 
@@ -7,26 +8,5 @@ def open_the_website(url):
     browser_lib.open_available_browser(url)
 
 
-def search_for(term):
-    input_field = "css:input"
-    browser_lib.input_text(input_field, term)
-    browser_lib.press_keys(input_field, "ENTER")
-
-
-def store_screenshot(filename):
-    browser_lib.screenshot(filename=filename)
-
-
-# Define a main() function that calls the other functions in order:
-def main():
-    try:
-        open_the_website("https://robocorp.com/docs/")
-        search_for("python")
-        store_screenshot("output/screenshot.png")
-    finally:
-        browser_lib.close_all_browsers()
-
-
-# Call the main() function, checking that we are running as a stand-alone script:
-if __name__ == "__main__":
-    main()
+def end_task():
+    browser_lib.close_all_browsers()
